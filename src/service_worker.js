ddc4b42ff9e74430124d5850f48ebab3acdc8562
daisuke motohashi <ddssk.m@gmail.com>
2023-02-20 09:24:37 +0900

    PWAの再設定

--- src/service_worker.js
+++ src/service_worker.js
@@ -1,27 +1,46 @@
 // キャッシュファイルの指定
 var CACHE_NAME = 'mailsmooth-pwa-caches';
-var urlsToCache = [
-    '/motohasystem.github.io/mail_smooth_web/',
+
+// Service Worker のバージョン
+const CACHE_VERSION = 1;
+
+// キャッシュするリソースのリスト
+const CACHE_LIST = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js'
 ];
 
-// インストール処理
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
-    );
+// Service Worker のインストール処理
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(`${CACHE_NAME}-v${CACHE_VERSION}`).then(cache => {
+      return cache.addAll(CACHE_LIST);
+    })
+  );
 });
 
-// リソースフェッチ時のキャッシュロード処理
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
-            })
-    );
+// Service Worker のアクティブ化処理
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => {
+          return !key.startsWith(`cache-v${CACHE_VERSION}`);
+        }).map(key => {
+          return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+// Service Worker のフェッチ処理
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
